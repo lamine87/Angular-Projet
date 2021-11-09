1bbeb45b92bfe93d604f1ab5d99b98ca3fb9bef6
lamine87 <laminenatidiarra@yahoo.fr>
2021-11-09 11:11:30 +0100

    Add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Import des composants utilisés dans la route
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+
+
+// Array contenant les routes de l'application (json)
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: ListComponent },
+
+  // Redirection vers home du lancement de l'application 
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
